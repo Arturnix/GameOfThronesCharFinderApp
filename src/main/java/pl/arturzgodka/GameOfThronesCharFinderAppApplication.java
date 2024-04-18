b083401fcfcc04b8c53267d2525b6d5d84961097
Arturnix <artur.zgodka.programista@gmail.com>
2024-04-18 23:00:09 +0200

    Add project structure

---
+++ src/main/java/pl/arturzgodka/GameOfThronesCharFinderAppApplication.java
@@ -0,0 +1,13 @@
+package pl.arturzgodka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameOfThronesCharFinderAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameOfThronesCharFinderAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/arturzgodka/GameOfThronesCharFinderAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.arturzgodka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameOfThronesCharFinderAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
