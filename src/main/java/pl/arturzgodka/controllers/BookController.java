472cd31a294e81845f3e2e57d5b4421f06a04124
Arturnix <artur.zgodka.programista@gmail.com>
2024-04-20 00:00:08 +0200

    Add show book details link

--- src/main/java/pl/arturzgodka/controllers/BookController.java
+++ src/main/java/pl/arturzgodka/controllers/BookController.java
@@ -3,6 +3,7 @@ package pl.arturzgodka.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import pl.arturzgodka.connectivity.BooksAPIHandler;
 
 @Controller
@@ -16,5 +17,12 @@ public class BookController {
         //aby moc przekazac dane z backendu do frontendu potrzebuje modelu. Przez model przekazuje te dane.
         model.addAttribute("booksList", apiHandler.listAllBooks()); //podaje najpier jak ma sie dana nazywac, a pozniej skad wziac jej wartosc.
         return "books";
+    }
+
+    @RequestMapping("/book") //przekauzje w widoku tam w linku argument id ksiazki. Zeby to polaczyc musze otagowac @RequestParam w parametrach metody.
+    public String getSingleBook(Model model, @RequestParam Integer id) { //model jako parametr bo bede przekazywal pojedyncza ksiazke.
+        //nazwa tego parametru jest taka sama jak podany w linku w widoku gdzie jest generowany ten link do tego konkretnego zasobu.
+        model.addAttribute("book", apiHandler.getSingleBook(id)); //id jest wybrane przez uzytkownika przez klikniecie w link przy danej ksiazce.
+        return "book";
     }
 }
