944ec11be5a289960af9475ee38dfecf6f83b8dd
Arturnix <artur.zgodka.programista@gmail.com>
2024-04-20 13:27:49 +0200

    Add character search

---
+++ src/main/java/pl/arturzgodka/controllers/CharacterController.java
@@ -0,0 +1,29 @@
+package pl.arturzgodka.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import pl.arturzgodka.connectivity.CharactersAPIHandler;
+import pl.arturzgodka.datamodel.CharacterSearchObject;
+
+@Controller
+public class CharacterController {
+
+    private CharactersAPIHandler charactersAPIHandler = new CharactersAPIHandler();
+
+    //Get dostaje formularz, Post aby przeslac dane z formularza do API
+
+    @GetMapping("/characterSearch") //jesli user wejdzie na taki adres GET to zwroc mu widok z return.
+    public String getCharacterSearchView(Model model) { //ta metoda ma 2 przypadki: obsluga zapytan GET i POST.
+        model.addAttribute("characterSearchObject", new CharacterSearchObject());
+        return "characterSearch";
+    }
+
+    @PostMapping("/characterSearch") //w ramach dzialanai POST zostaje wyslany na ten adres z wartosciami jakie zostaly podane w formularzu.
+    public String getSpecificCharacter(Model model, CharacterSearchObject characterSearchObject) { //tu jest przekazywany efekt dzialania post, to co jest w ciele.
+        //wykorzystuje jego wartosc, tego parametru aby dzieki niemu wyszukac zadana postac
+        model.addAttribute("character", charactersAPIHandler.getBookCharacter(characterSearchObject.getName())); //wykorzystuje do wyszukania w RESTowym API bohatera o zadanym imieniu podanym w formularzu, to w tym obiekcie jest ono przechowywane.
+        return "character";
+    }
+}
